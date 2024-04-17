ab9194081ccc74f7fec7d40672e5aece97482569
Steven Su <lakewood999@gmail.com>
2024-04-16 22:10:40 -0400

    Fix missing import

--- ytdownloader/react_src/download_form.tsx
+++ ytdownloader/react_src/download_form.tsx
@@ -8,30 +8,7 @@ This program is distributed in the hope that it will be useful, but WITHOUT ANY 
 You should have received a copy of the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
 */
 import React from 'react';
-
-type DownloadStep = { number: number, text: string, suffix: string, stateDone: number, stateLoading: number };
-
-const DownloadStatusBox = (props: {
-    state: number,
-    steps: DownloadStep[],
-    error_code: string
-}) => {
-    let out = null;
-    if (props.state !== -1) {
-        out = props.steps.map(
-            (step) => <div key={step.number}><StatusLine itemNumber={step.number}
-                text={step.text}
-                suffix={step.suffix}
-                stateNumber={props.state}
-                stateDone={step.stateDone}
-                stateLoading={step.stateLoading}
-            /><br /></div>
-        )
-    } else {
-        out = <span className="has-text-danger"><b>Message: </b>An error has occurred. Please check URL or contact an administrator. Error details: {props.error_code}</span>;
-    }
-    return <div>{out}</div>;
-}
+import DownloadStatusBox from './DownloadStatusBox';
 
 class DownloadForm extends React.Component {
     constructor(props) {
